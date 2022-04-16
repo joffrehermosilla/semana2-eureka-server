eec5e50ba72d50df193ec2a0701969759e9ac07b
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-15 23:44:35 -0500

    Primer commit configuracion Docker con eureka server

---
+++ microservicio-eureka-server/src/test/java/nttdata/bootcamp/microservicios/eureka/MicroservicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
